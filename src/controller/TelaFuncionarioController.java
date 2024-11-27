883a8121a13706c2b9b746f6954b22c36584e7db
luisMigue-lm <tavaresalvesluismiguel@gmail.com>
2024-11-27 20:36:10 -0300

    Finalização da tela dos funcionários e inserção do Controller da tela.

---
+++ src/controller/TelaFuncionarioController.java
@@ -0,0 +1,53 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
+public class TelaFuncionarioController {
+
+    @FXML
+    private BorderPane bPane;
+
+    @FXML
+    private Button btCadastrar;
+
+    @FXML
+    private Button btDeletar;
+
+    @FXML
+    private Button btOpcoes;
+
+    @FXML
+    private Button btPesquisar;
+
+    @FXML
+    private Button btVoltar;
+
+    @FXML
+    void btCadastrarClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btDeletarClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btOpcoesClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btPesquisarClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btVoltarClick(ActionEvent event) {
+
+    }
+
+}
